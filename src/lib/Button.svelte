1430d5aa56deb3df41ebb6353ab56c190157c5dc
Zachiah Sawyer <zachiah@proton.me>
2025-03-27 08:43:17 -0700

    Improve disabled buttons

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -14,14 +14,18 @@
 	} = $props();
 
 	const classStyles = {
-		primary: 'bg-blue-500',
-		danger: 'bg-red-500'
+		primary: 'cursor-pointer bg-blue-500',
+		danger: 'cursor-pointer bg-red-500'
+	};
+	const disabledClassStyles = {
+		primary: 'bg-blue-500/80 cursor-not-allowed text-gray-800',
+		danger: 'bg-red-500/80 cursor-not-allowed text-gray-800'
 	};
 </script>
 
 <button
 	{disabled}
-	class={`cursor-pointer rounded-md px-4 py-2 ${classStyles[style]}`}
+	class={`rounded-md px-4 py-2 ${disabled ? disabledClassStyles[style] : classStyles[style]}`}
 	onclick={onClick}
 >
 	{@render children()}
