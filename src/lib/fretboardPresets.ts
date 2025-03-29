0036f3a65ebba70f9997b091f9c40ed39bd64b83
Zachiah Sawyer <zachiah@proton.me>
2025-03-29 12:31:24 -0700

    Fix broken preset picking

---
+++ src/lib/fretboardPresets.ts
@@ -0,0 +1,58 @@
+import { CanonicalPitch } from './CanonicalPitch';
+import type { Fretboard } from './Fretboard';
+import { generateId } from './generateId';
+
+const guitarDots = [
+	null,
+	null,
+	1,
+	null,
+	1,
+	null,
+	1,
+	null,
+	1,
+	null,
+	null,
+	2,
+	null,
+	null,
+	1,
+	null,
+	1,
+	null,
+	1,
+	null,
+	1
+];
+
+const ukeleleDots = [null, null, 1, null, 1, null, 1, null, null, 1, null, 1];
+
+export const defaultPresets: Fretboard[] = [
+	{
+		name: 'Guitar',
+		strings: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
+		dots: guitarDots
+	},
+	{ name: 'Drop D Guitar', strings: ['D2', 'A2', 'D3', 'G3', 'B3', 'E2'], dots: guitarDots },
+	{ name: 'DADGAD', strings: ['D2', 'A2', 'D3', 'G3', 'A3', 'D4'], dots: guitarDots },
+	{
+		name: 'Ukelele (reentrant)',
+		strings: ['G4', 'C4', 'E4', 'A4'],
+		frets: 18,
+		dots: ukeleleDots
+	},
+	{ name: 'Ukelele (low G)', strings: ['G3', 'C4', 'E4', 'A4'], frets: 18, dots: ukeleleDots }
+].map((row) => ({
+	id: generateId(),
+	frets: 24,
+	...row,
+	strings: row.strings.map((s) => {
+		const parsed = CanonicalPitch.parse(s);
+		if (!parsed) {
+			throw new Error('Failed to parse');
+		}
+
+		return parsed;
+	})
+}));
--- src/routes/chord-identifier/[instrument=instrument]/FretboardChordIdentifier.svelte
+++ src/routes/chord-identifier/[instrument=instrument]/FretboardChordIdentifier.svelte
@@ -7,7 +7,7 @@
 	import type { Fretboard } from '$lib/Fretboard';
 	import { createLocalStorageState } from '$lib/localStorageState.svelte';
 	import FretboardSelector from '$lib/FretboardSelector.svelte';
-	import { generateId } from '$lib/generateId';
+	import { defaultPresets } from '$lib/fretboardPresets';
 
 	const {
 		options,
@@ -16,61 +16,6 @@
 		options: GuessedChord.PrintingOptions;
 		onOptionsChange(o: GuessedChord.PrintingOptions): void;
 	} = $props();
-
-	const guitarDots = [
-		null,
-		null,
-		1,
-		null,
-		1,
-		null,
-		1,
-		null,
-		1,
-		null,
-		null,
-		2,
-		null,
-		null,
-		1,
-		null,
-		1,
-		null,
-		1,
-		null,
-		1
-	];
-
-	const ukeleleDots = [null, null, 1, null, 1, null, 1, null, null, 1, null, 1];
-
-	const defaultPresets: Fretboard[] = [
-		{
-			name: 'Guitar',
-			strings: ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'],
-			dots: guitarDots
-		},
-		{ name: 'Drop D Guitar', strings: ['D2', 'A2', 'D3', 'G3', 'B3', 'E2'], dots: guitarDots },
-		{ name: 'DADGAD', strings: ['D2', 'A2', 'D3', 'G3', 'A3', 'D4'], dots: guitarDots },
-		{
-			name: 'Ukelele (reentrant)',
-			strings: ['G4', 'C4', 'E4', 'A4'],
-			frets: 18,
-			dots: ukeleleDots
-		},
-		{ name: 'Ukelele (low G)', strings: ['G3', 'C4', 'E4', 'A4'], frets: 18, dots: ukeleleDots }
-	].map((row) => ({
-		id: generateId(),
-		frets: 24,
-		...row,
-		strings: row.strings.map((s) => {
-			const parsed = CanonicalPitch.parse(s);
-			if (!parsed) {
-				throw new Error('Failed to parse');
-			}
-
-			return parsed;
-		})
-	}));
 
 	let fretboardPresets = createLocalStorageState<Fretboard[]>(
 		'fretboardPresets',
