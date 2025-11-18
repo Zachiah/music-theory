7b5cefec1630a2b5c8795ebce7c8246b1d3cf1ef
Zachiah Sawyer <zachiah@proton.me>
2025-11-18 11:01:15 -0800

    Add links to github and my site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,14 @@
 		<li class="contents">
 			<a class="p-4 hover:text-blue-500" href="/chord-identifier/keyboard">Identify</a>
 		</li>
+
+		<span class="flex-grow"></span>
+
+		<li class="contants">
+			<a class="flex h-full items-center justify-center p-4 hover:text-blue-500" href="https://github.com/Zachiah/music-theory" aria-label="GitHub">
+				<span class="size-6 icon-[octicon--mark-github-16]"></span>
+			</a>
+		</li>
 	</ul>
 </nav>
 
