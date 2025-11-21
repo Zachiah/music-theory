b894215ae3cd3c5f8252a4b852f448905e4148d3
Zachiah Sawyer <zachiah@proton.me>
2025-11-20 23:51:06 -0800

    Fix dialog drag outside glitch

--- src/lib/ModalDialogBase.svelte
+++ src/lib/ModalDialogBase.svelte
@@ -11,19 +11,38 @@
 		onClose(): void;
 		open: boolean;
 	} = $props();
+
+	let pressedInside = $state(false);
 </script>
 
 {#if open}
 	<div
 		role="presentation"
 		class="fixed top-0 right-0 bottom-0 left-0 z-50 flex h-full w-full items-center justify-center bg-gray-800/80 dark:bg-slate-600/80"
+		onmouseup={() => {
+			setTimeout(() => {
+				pressedInside = false;
+			});
+		}}
 		onclick={(e) => {
+			if (pressedInside) {
+				return;
+			}
+
 			if (e.currentTarget === e.target) {
 				onClose();
 			}
 		}}
 		transition:fade={{ duration: 100 }}
 	>
-		{@render children()}
+		<!-- svelte-ignore a11y_no_static_element_interactions -->
+		<div
+			class="absolute flex items-center justify-center"
+			onmousedown={() => {
+				pressedInside = true;
+			}}
+		>
+			{@render children()}
+		</div>
 	</div>
 {/if}
