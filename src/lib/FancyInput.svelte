37e2a2cffb9519e4b015183974986b895056dd77
Zachiah Sawyer <zachiah@proton.me>
2025-11-20 23:18:06 -0800

    Add animation to fancy input

--- src/lib/FancyInput.svelte
+++ src/lib/FancyInput.svelte
@@ -4,7 +4,7 @@
 
 <label class="group relative flex-1 rounded-md dark:bg-slate-600">
 	<p
-		class="absolute left-3 transform text-gray-300 group-focus-within:top-0.5 group-focus-within:translate-y-0 group-focus-within:text-xs dark:text-slate-300 {value
+		class="absolute left-3 transform text-gray-300 duration-200 group-focus-within:top-0.5 group-focus-within:translate-y-0 group-focus-within:text-xs dark:text-slate-300 {value
 			? 'top-0.5 translate-y-0 text-xs'
 			: 'top-1/2 -translate-y-1/2'}"
 	>
