48c69de6044a84036f53f143dd890c98a17a51ac
Zachiah Sawyer <zachiah@proton.me>
2025-12-11 13:33:13 -0800

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+	import Container from '$lib/Container.svelte';
+</script>
+
+<Container>
+	<div class="flex flex-col items-center justify-center gap-4 p-8">
+		<h1 class="text-4xl">{page.status} {page.error?.message}</h1>
+		<p class="text-2xl">
+			<a href={resolve('/games/piano')} class="text-blue-400">Play a piano with your keyboard</a>
+		</p>
+	</div>
+</Container>
